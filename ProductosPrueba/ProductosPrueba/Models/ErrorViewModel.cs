9efc9a0fe8f9fb42cd18650da378dc0786afcdab
SORAYA VALENTINA RETAMOZO GUILLEN <valentinarguillen@gmail.com>
2023-04-26 12:32:46 -0500

    Capa2

---
+++ ProductosPrueba/ProductosPrueba/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductosPrueba.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
